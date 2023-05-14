1e07435bf9eddd7e693480bd36c22b8e3ffc7699
jeongyeonKimm <wjddus9132@gmail.com>
2023-05-15 01:59:38 +0900

    feat: 성별 선택 버튼 구현

---
+++ android/app/src/main/kotlin/com/example/dr_closet/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dr_closet
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          color: Color(0xFFEEEFF1),
+        ),
+        padding: const EdgeInsets.symmetric(horizontal: 71),
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Text(
+                  '성별을 선택해주세요',
+                  style: TextStyle(
+                    fontSize: 30,
+                    color: Color(0xFF6B97E9),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 132,
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color(0xFFECF0F3),
+                foregroundColor: const Color(0xFF6B97E9),
+                padding: const EdgeInsets.symmetric(
+                  vertical: 27.0,
+                ),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                elevation: 25,
+                shadowColor: const Color(0xFF6291E8).withOpacity(0.5),
+              ),
+              child: const Text(
+                '여성',
+                style: TextStyle(
+                  fontSize: 30,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 60,
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color(0xFFECF0F3),
+                foregroundColor: const Color(0xFF6B97E9),
+                padding: const EdgeInsets.symmetric(
+                  vertical: 27.0,
+                ),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                elevation: 25,
+                shadowColor: const Color(0xFF6291E8).withOpacity(0.5),
+              ),
+              child: const Text(
+                '남성',
+                style: TextStyle(
+                  fontSize: 30,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
